8b06d45fa056f1fc82ba70ded73fe084ebf3904a
wyx1413xyd <3060491854@qq.com>
2024-04-26 14:36:21 +0800

    小程序页面代码

---
+++ errand_app/pages/personal/personal.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<!-- 个人信息 -->
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
