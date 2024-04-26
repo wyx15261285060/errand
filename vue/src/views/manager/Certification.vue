733eba5b3cc1587a800fe07bcc3edd4690622465
wyx1413xyd <3060491854@qq.com>
2024-04-26 14:28:37 +0800

    小程序管理平台前端页面代码

---
+++ vue/src/views/manager/Certification.vue
@@ -0,0 +1,192 @@
+<template>
+  <div>
+    <div class="search">
+      <el-input placeholder="请输入骑手名称查询" style="width: 200px" v-model="name"></el-input>
+      <el-button type="info" plain style="margin-left: 10px" @click="load(1)">查询</el-button>
+      <el-button type="warning" plain style="margin-left: 10px" @click="reset">重置</el-button>
+    </div>
+
+    <div class="operation">
+      <el-button type="danger" plain @click="delBatch">批量删除</el-button>
+    </div>
+
+    <div class="table">
+      <el-table :data="tableData" strip @selection-change="handleSelectionChange">
+        <el-table-column type="selection" width="55" align="center"></el-table-column>
+        <el-table-column prop="id" label="序号" width="70" align="center" sortable></el-table-column>
+        <el-table-column prop="userId" label="账号ID"></el-table-column>
+        <el-table-column prop="name" label="名称"></el-table-column>
+        <el-table-column label="本人照片">
+          <template v-slot="scope">
+          <el-image :src="scope.row.avatar" style="width: 50px; border-radius: 5px"
+            :preview-src-list="[scope.row.avatar]"></el-image>
+        </template>
+        </el-table-column>
+        <el-table-column prop="phone" label="联系方式"></el-table-column>
+        <el-table-column prop="cardNo" label="身份证号码"></el-table-column>
+        <el-table-column prop="card1" label="身份证正面">
+          <template v-slot="scope">
+          <el-image :src="scope.row.card1" style="width: 50px; border-radius: 5px"
+            :preview-src-list="[scope.row.card1]"></el-image>
+        </template>
+        </el-table-column>
+        <el-table-column prop="card2" label="身份证反面">
+          <template v-slot="scope">
+          <el-image :src="scope.row.card2" style="width: 50px; border-radius: 5px"
+            :preview-src-list="[scope.row.card2]"></el-image>
+        </template>
+        </el-table-column>
+        <el-table-column prop="address" label="常住地址"></el-table-column>
+        <el-table-column prop="status" label="审核状态">
+          <template v-slot="scope">
+            <el-tag type="info" v-if="scope.row.status === '待审核'">待审核</el-tag>
+            <el-tag type="success" v-if="scope.row.status === '通过'">通过</el-tag>
+            <el-tag type="danger" v-if="scope.row.status === '拒绝'">拒绝</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column prop="reason" label="审核理由"></el-table-column>
+        <el-table-column label="操作" align="center" width="180">
+          <template v-slot="scope">
+            <el-button size="mini" type="primary" plain @click="handleEdit(scope.row)">审核</el-button>
+            <el-button size="mini" type="danger" plain @click="del(scope.row.id)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <div class="pagination">
+        <el-pagination background @current-change="handleCurrentChange" :current-page="pageNum"
+          :page-sizes="[5, 10, 20]" :page-size="pageSize" layout="total, prev, pager, next" :total="total">
+        </el-pagination>
+      </div>
+    </div>
+
+    <el-dialog title="信息" :visible.sync="fromVisible" width="40%" :close-on-click-modal="false" destroy-on-close>
+      <el-form :model="form" label-width="100px" style="padding-right: 50px" :rules="rules" ref="formRef">
+        <el-form-item label="名称" prop="name">
+          <el-input v-model="form.name" placeholder="名称" disabled></el-input>
+        </el-form-item>
+        <el-form-item label="审核状态" prop="status">
+          <el-select style="width: 100%" v-model="form.status">
+            <el-option v-for="item in ['待审核', '通过', '拒绝']" :key="item" :value="item" :label="item"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="审核理由" prop="reason">
+          <el-input type="textarea" v-model="form.reason" placeholder="审核理由"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="fromVisible = false">取 消</el-button>
+        <el-button type="primary" @click="save">确 定</el-button>
+      </div>
+    </el-dialog>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Certification",
+  data() {
+    return {
+      tableData: [],  // 所有的数据
+      pageNum: 1,   // 当前的页码
+      pageSize: 10,  // 每页显示的个数
+      total: 0,
+      name: null,
+      fromVisible: false,
+      form: {},
+      user: JSON.parse(localStorage.getItem('xm-user') || '{}'),
+      rules: {},
+      ids: []
+    }
+  },
+  created() {
+    this.load(1)
+  },
+  methods: {
+    handleAdd() {   // 新增数据
+      this.form = {}  // 新增数据的时候清空数据
+      this.fromVisible = true   // 打开弹窗
+    },
+    handleEdit(row) {   // 编辑数据
+      this.form = JSON.parse(JSON.stringify(row))  // 给form对象赋值  注意要深拷贝数据
+      this.fromVisible = true   // 打开弹窗
+    },
+    save() {   // 保存按钮触发的逻辑  它会触发新增或者更新
+      this.$refs.formRef.validate((valid) => {
+        if (valid) {
+          this.$request({
+            url: this.form.id ? '/certification/update' : '/certification/add',
+            method: this.form.id ? 'PUT' : 'POST',
+            data: this.form
+          }).then(res => {
+            if (res.code === '200') {  // 表示成功保存
+              this.$message.success('保存成功')
+              this.load(1)
+              this.fromVisible = false
+            } else {
+              this.$message.error(res.msg)  // 弹出错误的信息
+            }
+          })
+        }
+      })
+    },
+    del(id) {   // 单个删除
+      this.$confirm('您确定删除吗？', '确认删除', { type: "warning" }).then(response => {
+        this.$request.delete('/certification/delete/' + id).then(res => {
+          if (res.code === '200') {   // 表示操作成功
+            this.$message.success('操作成功')
+            this.load(1)
+          } else {
+            this.$message.error(res.msg)  // 弹出错误的信息
+          }
+        })
+      }).catch(() => {
+      })
+    },
+    handleSelectionChange(rows) {   // 当前选中的所有的行数据
+      this.ids = rows.map(v => v.id)   //  [1,2]
+    },
+    delBatch() {   // 批量删除
+      if (!this.ids.length) {
+        this.$message.warning('请选择数据')
+        return
+      }
+      this.$confirm('您确定批量删除这些数据吗？', '确认删除', { type: "warning" }).then(response => {
+        this.$request.delete('/certification/delete/batch', { data: this.ids }).then(res => {
+          if (res.code === '200') {   // 表示操作成功
+            this.$message.success('操作成功')
+            this.load(1)
+          } else {
+            this.$message.error(res.msg)  // 弹出错误的信息
+          }
+        })
+      }).catch(() => {
+      })
+    },
+    load(pageNum) {  // 分页查询
+      if (pageNum) this.pageNum = pageNum
+      this.$request.get('/certification/selectPage', {
+        params: {
+          pageNum: this.pageNum,
+          pageSize: this.pageSize,
+          name: this.name,
+        }
+      }).then(res => {
+        this.tableData = res.data?.list
+        this.total = res.data?.total
+      })
+    },
+    reset() {
+      this.name = null
+      this.load(1)
+    },
+    handleCurrentChange(pageNum) {
+      this.load(pageNum)
+    },
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ vue/src/views/manager/Order.vue
@@ -0,0 +1,221 @@
+<template>
+  <!-- 用户管理页面 -->
+  <div>
+    <!-- 搜索框 -->
+    <div class="search">
+      <el-input placeholder="请输入订单编号查询" style="width: 200px" v-model="OrderNo"></el-input>
+      <el-button type="info" plain style="margin-left: 10px" @click="load(1)">查询</el-button>
+      <el-button type="warning" plain style="margin-left: 10px" @click="reset">重置</el-button>
+    </div>
+    <div class="operation">
+      <el-button type="danger" plain @click="delBatch">批量删除</el-button>
+    </div>
+    <!-- 表格栏 -->
+    <div class="table">
+      <el-table :data="tableData" strip @selection-change="handleSelectionChange">
+        <el-table-column type="selection" width="55" align="center"></el-table-column>
+        <el-table-column prop="id" label="序号" width="70" align="center" sortable></el-table-column>
+        <el-table-column prop="orderNo" label="订单编号"></el-table-column>
+        <el-table-column prop="name" label="物品名称"></el-table-column>
+        <el-table-column prop="descr" label="描述"></el-table-column>
+        <el-table-column prop="img" label="物品图片"> 
+        <template v-slot="scope">
+          <el-image :src="scope.row.img" style="width: 50px; border-radius: 5px"
+            :preview-src-list="[scope.row.img]"></el-image>
+        </template>
+      </el-table-column>
+        <el-table-column prop="type" label="物品类型"></el-table-column>
+        <el-table-column prop="weight" label="物品重量"></el-table-column>
+        <el-table-column prop="price" label="小费"></el-table-column>
+        <el-table-column prop="userName" label="发起人"></el-table-column>
+        <el-table-column prop="acceptName" label="接单人"></el-table-column>
+        <el-table-column prop="time" label="创建时间"></el-table-column>
+        <el-table-column prop="acceptTime" label="接单时间"></el-table-column>
+        <el-table-column prop="status" label="订单状态">
+          <template v-slot="scope">
+            <el-tag type="danger" v-if="scope.row.status === '已取消'">已取消</el-tag>
+            <el-tag type="Info" v-if="scope.row.status === '待接单'">待接单</el-tag>
+            <el-tag type="primary" v-if="scope.row.status === '待送达'">待送达</el-tag>
+            <el-tag type="primary" v-if="scope.row.status === '待收货'">待收货</el-tag>
+            <el-tag type="warning" v-if="scope.row.status === '待评价'">待评价</el-tag>
+            <el-tag type="success" v-if="scope.row.status === '已完成'">已完成</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column prop="addressId" label="取货信息">
+          <template v-slot="scope">
+            <el-button type="parimary" size="mini" @click = showAddress(scope.row.addressId)>查看</el-button>
+          </template>
+        </el-table-column>
+        <el-table-column prop="targetId" label="送货信息">
+          <template v-slot="scope">
+            <el-button type="parimary" size="mini"@click = showAddress(scope.row.targetId)>查看</el-button>
+          </template>
+        </el-table-column>
+        <el-table-column prop="comment" label="订单备注"></el-table-column>
+        <el-table-column label="操作" align="center" width="150" fixed="right">
+          <template v-slot="scope">
+            <el-button size="mini" type="primary" plain @click="handleEdit(scope.row)">编辑</el-button>
+            <el-button size="mini" type="danger" plain @click="del(scope.row.id)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <div class="pagination">
+        <el-pagination background @current-change="handleCurrentChange" :current-page="pageNum"
+          :page-sizes="[5, 10, 20]" :page-size="pageSize" layout="total, prev, pager, next" :total="total">
+        </el-pagination>
+      </div>
+    </div>
+    <el-dialog title="信息" :visible.sync="fromVisible" width="40%" :close-on-click-modal="false" destroy-on-close>
+      <el-form label-width="100px" style="padding-right: 50px" :model="form">
+        <el-form-item prop="status" label="订单状态">
+          <el-select v-model="form.status" style="width: 100%">
+            <el-option value="已取消"></el-option>
+            <el-option value="待接单"></el-option>
+            <el-option value="待送达"></el-option>
+            <el-option value="待收货"></el-option>
+            <el-option value="待评价"></el-option>
+            <el-option value="已完成"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="fromVisible = false">取 消</el-button>
+        <el-button type="primary" @click="save">确 定</el-button>
+      </div>
+    </el-dialog>
+
+    <el-dialog title="地址信息" :visible.sync="fromVisible1" width="40%" :close-on-click-modal="false" destroy-on-close>
+      <el-form label-width="100px" style="padding-right: 50px" :model="form">
+        <el-form-item  label="地址: ">
+          <div>{{ address.address }}</div>
+        </el-form-item>
+        <el-form-item  label="门牌号: ">
+          <div>{{ address.doorNo }}</div>
+        </el-form-item>
+        <el-form-item  label="联系人: ">
+          <div>{{ address.userName }}</div>
+        </el-form-item>
+        <el-form-item  label="联系电话: ">
+          <div>{{ address.phone }}</div>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="fromVisible1 = false">关 闭</el-button>
+      </div>
+    </el-dialog>
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Order",
+  data() {
+    return {
+      tableData: [],  // 所有的数据
+      pageNum: 1,   // 当前的页码
+      pageSize: 10,  // 每页显示的个数
+      total: 0,
+      OrderNo: null,
+      form: {},
+      fromVisible: false,
+      fromVisible1:false,
+      order: JSON.parse(localStorage.getItem('xm-order') || '{}'),
+      ids: [],
+      address:{},
+    }
+  },
+  created() {
+    this.load(1)
+  },
+  methods: {
+    showAddress(addressId){
+      this.$request.get('/address/selectById/ '+ addressId)
+      .then(res => {
+        this.address = res.data || {};
+        this.fromVisible1 = true;
+      })
+    },
+    handleEdit(row) {   // 编辑订单状态 
+      this.form = JSON.parse(JSON.stringify(row))  // 给form对象赋值  注意要深拷贝数据
+      this.fromVisible = true   // 打开弹窗
+    },
+    save() {   // 保存按钮触发的逻辑  它会触发新增或者更新
+      this.$request({
+        url: this.form.id ? '/order/update' : '/order/add',
+        method: this.form.id ? 'PUT' : 'POST',
+        data: this.form
+      }).then(res => {
+        if (res.code === '200') {  // 表示成功保存
+          this.$message.success('保存成功')
+          this.load(1)
+          this.fromVisible = false
+        } else {
+          this.$message.error(res.msg)  // 弹出错误的信息
+        }
+      })
+    },
+    del(id) {   // 单个删除
+      this.$confirm('您确定删除吗？', '确认删除', { type: "warning" }).then(response => {
+        this.$request.delete('/order/delete/' + id).then(res => {
+          if (res.code === '200') {   // 表示操作成功
+            this.$message.success('操作成功')
+            this.load(1)
+          } else {
+            this.$message.error(res.msg)  // 弹出错误的信息
+          }
+        })
+      }).catch(() => {
+      })
+    },
+    // 当前选中的所有的行数据
+    handleSelectionChange(rows) {
+      // map函数将选中的行的id值转换为一个数组，并将该数组赋值给this.ids变量
+      this.ids = rows.map(v => v.id)
+    },
+    delBatch() {   // 批量删除
+      if (!this.ids.length) {
+        this.$message.warning('请选择数据')
+        return
+      }
+      this.$confirm('您确定批量删除这些数据吗？', '确认删除', { type: "warning" }).then(response => {
+        this.$request.delete('/order/delete/batch', { data: this.ids }).then(res => {
+          if (res.code === '200') {   // 表示操作成功
+            this.$message.success('操作成功')
+            this.load(1)
+          } else {
+            this.$message.error(res.msg)  // 弹出错误的信息
+          }
+        })
+      }).catch(() => {
+      })
+    },
+    load(pageNum) {  // 分页查询
+      if (pageNum) this.pageNum = pageNum
+      this.$request.get('/order/selectPage', {
+        params: {
+          pageNum: this.pageNum,
+          pageSize: this.pageSize,
+          OrderNo: this.OrderNo,
+        }
+      }).then(res => {
+        // ?. 是可选链操作符
+        this.tableData = res.data?.list
+        this.total = res.data?.total
+      })
+    },
+    reset() {
+      this.OrderNo = null
+      this.load(1);
+    },
+    handleCurrentChange(pageNum) {
+      this.load(pageNum)
+    },
+  }
+}
+</script>
+
+<style scoped></style>
