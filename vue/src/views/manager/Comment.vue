520762554130cd33c3605893dd7d7dc0cfb03f14
wyx1413xyd <3060491854@qq.com>
2024-04-27 10:55:46 +0800

    【开发】: 评价信息管理模块前端

---
+++ vue/src/views/manager/Comment.vue
@@ -0,0 +1,140 @@
+<template>
+  <div>
+    <div class="search">
+      <el-input placeholder="请输入评价内容称查询" style="width: 200px" v-model="content"></el-input>
+      <el-input placeholder="请输入用户名称查询" style="width: 200px; margin: 0 10px" v-model="userName"></el-input>
+      <el-input placeholder="请输入骑手名称查询" style="width: 200px; margin-right: 10px" v-model="acceptName"></el-input>
+      <el-input placeholder="请输入订单编号查询" style="width: 200px" v-model="orderNo"></el-input>
+      <el-button type="info" plain style="margin-left: 10px" @click="load(1)">查询</el-button>
+      <el-button type="warning" plain style="margin-left: 10px" @click="reset">重置</el-button>
+    </div>
+
+    <div class="operation">
+      <el-button type="danger" plain @click="delBatch">批量删除</el-button>
+    </div>
+
+    <el-table :data="tableData" strip @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center"></el-table-column>
+      <el-table-column prop="id" label="序号" width="70" align="center" sortable></el-table-column>
+      <el-table-column prop="content" label="评论"></el-table-column>
+      <el-table-column prop="star" label="评分">
+        <template v-slot="scope">
+          <el-rate v-model="scope.row.star" disabled></el-rate>
+        </template>
+      </el-table-column>
+      <el-table-column prop="userName" label="用户"></el-table-column>
+      <el-table-column prop="acceptName" label="骑手"></el-table-column>
+      <el-table-column prop="orderId" label="订单ID"></el-table-column>
+      <el-table-column prop="time" label="时间"></el-table-column>
+      <el-table-column label="操作" align="center" width="180">
+        <template v-slot="scope">
+          <el-button size="mini" type="danger" plain @click="del(scope.row.id)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+      <div class="pagination">
+        <el-pagination
+            background
+            @current-change="handleCurrentChange"
+            :current-page="pageNum"
+            :page-sizes="[5, 10, 20]"
+            :page-size="pageSize"
+            layout="total, prev, pager, next"
+            :total="total">
+        </el-pagination>
+      </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: "comment",
+  data() {
+    return {
+      tableData: [],  // 所有的数据
+      pageNum: 1,   // 当前的页码
+      pageSize: 10,  // 每页显示的个数
+      total: 0,
+      content: null,
+      userName: null,
+      acceptName: null,
+      orderNo: null,
+      fromVisible: false,
+      form: {},
+      user: JSON.parse(localStorage.getItem('xm-user') || '{}'),
+      rules:{},
+      ids: []
+    }
+  },
+  created() {
+    this.load(1)
+  },
+  methods: {
+    del(id) {   // 单个删除
+      this.$confirm('您确定删除吗？', '确认删除', {type: "warning"}).then(response => {
+        this.$request.delete('/comment/delete/' + id).then(res => {
+          if (res.code === '200') {   // 表示操作成功
+            this.$message.success('操作成功')
+            this.load(1)
+          } else {
+            this.$message.error(res.msg)  // 弹出错误的信息
+          }
+        })
+      }).catch(() => {
+      })
+    },
+    handleSelectionChange(rows) {   // 当前选中的所有的行数据
+      this.ids = rows.map(v => v.id)   //  [1,2]
+    },
+    delBatch() {   // 批量删除
+      if (!this.ids.length) {
+        this.$message.warning('请选择数据')
+        return
+      }
+      this.$confirm('您确定批量删除这些数据吗？', '确认删除', {type: "warning"}).then(response => {
+        this.$request.delete('/comment/delete/batch', {data: this.ids}).then(res => {
+          if (res.code === '200') {   // 表示操作成功
+            this.$message.success('操作成功')
+            this.load(1)
+          } else {
+            this.$message.error(res.msg)  // 弹出错误的信息
+          }
+        })
+      }).catch(() => {
+      })
+    },
+    load(pageNum) {  // 分页查询
+      if (pageNum) this.pageNum = pageNum
+      this.$request.get('/comment/selectPage', {
+        params: {
+          pageNum: this.pageNum,
+          pageSize: this.pageSize,
+          content: this.content,
+          userName: this.userName,
+          acceptName: this.acceptName,
+          orderNo: this.orderNo,
+        }
+      }).then(res => {
+        this.tableData = res.data?.list
+        this.total = res.data?.total
+      })
+    },
+    reset() {
+      this.content = null
+      this.userName = null
+      this.acceptName = null
+      this.orderNo = null
+      this.load(1)
+    },
+    handleCurrentChange(pageNum) {
+      this.load(pageNum)
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
